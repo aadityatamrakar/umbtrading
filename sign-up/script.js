654379313d0f354552fb65231dc67725afd55c30
Aaditya Tamrakar <aaditya.span@gmail.com>
2021-12-11 20:23:11 -0500

    form validation

--- sign-up/script.js
+++ sign-up/script.js
@@ -4,34 +4,45 @@ const signUp = e => {
         email = document.getElementById("Email").value,
         password = document.getElementById("password").value;
 
-    let form_data = JSON.parse(localStorage.getItem('form_data')) || [];
+    let error_msg = '';
+    
+    // form validation
+    if (firstname.length == 0) error_msg += 'First Name is required.\n';
+    if (lastname.length == 0) error_msg += 'Last Name is required.\n';
+    if (email.length == 0) error_msg += 'Email is required.\n';
+    if (password.length == 0) error_msg += 'Password is required.\n';
 
-    let exist = form_data.length &&
-        JSON.parse(localStorage.getItem('form_data')).some(data => {
-            if (data.email.toLowerCase() == email && data.password.toLowerCase() == password) {
-                localStorage.setItem('current_user', JSON.stringify(data));
-                return true;
-            } else return false;
-        });
+    if (error_msg.length > 0) alert(error_msg);
+    else {
+        let form_data = JSON.parse(localStorage.getItem('form_data')) || [];
 
-    if (!exist) {
-        let current_user = {
-            firstname,
-            lastname,
-            email,
-            password
-        };
-        form_data.push(current_user);
-        localStorage.setItem('form_data', JSON.stringify(form_data));
-        localStorage.setItem('current_user', JSON.stringify(current_user));
-        document.querySelector('form').reset();
-        document.getElementById("Email").focus();
+        let exist = form_data.length &&
+            JSON.parse(localStorage.getItem('form_data')).some(data => {
+                if (data.email.toLowerCase() == email && data.password.toLowerCase() == password) {
+                    localStorage.setItem('current_user', JSON.stringify(data));
+                    return true;
+                } else return false;
+            });
 
-        // alert("Account Created.\n\nPlease Sign In using the link below.");
-        window.location.href = "./landing-page.html";
-    } else {
-        alert("You account already exist, Logging in using same credentials...");
-        window.location.href = "./landing-page.html";
+        if (!exist) {
+            let current_user = {
+                firstname,
+                lastname,
+                email,
+                password
+            };
+            form_data.push(current_user);
+            localStorage.setItem('form_data', JSON.stringify(form_data));
+            localStorage.setItem('current_user', JSON.stringify(current_user));
+            document.querySelector('form').reset();
+            document.getElementById("Email").focus();
+
+            // alert("Account Created.\n\nPlease Sign In using the link below.");
+            window.location.href = "./landing-page.html";
+        } else {
+            alert("You account already exist, Logging in using same credentials...");
+            window.location.href = "./landing-page.html";
+        }
     }
     e.preventDefault();
 }
